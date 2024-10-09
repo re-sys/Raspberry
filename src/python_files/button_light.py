ed7e6c3a2ea80963f337520f44b3145a10ac8076
wu <2565896577@qq.com>
2024-10-09 09:52:49 +0800

    change button detect with always detect

--- src/python_files/button_light.py
+++ src/python_files/button_light.py
@@ -8,12 +8,21 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led_green, GPIO.OUT)
 GPIO.setup(led_red, GPIO.OUT)
 GPIO.setup(button, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+button_pressed = True
+def sleep_and_wait_for_button_press(time_to_sleep):
+    for i in range(int(time_to_sleep*100)):
+        if GPIO.input(button) == button_pressed:
+            return True
+        time.sleep(0.01)
+
+
 def green_toggle():
     GPIO.output(led_green, 1)
     GPIO.output(led_red, 0)
-    time.sleep(0.1)
+    sleep_and_wait_for_button_press(0.5)
     GPIO.output(led_green, 0)
-    time.sleep(0.1)
+    sleep_and_wait_for_button_press(0.5)
 def red():
     GPIO.output(led_green, 0)
     GPIO.output(led_red, 1)
@@ -21,9 +30,9 @@ def red():
 def red_toggle():
     GPIO.output(led_green, 0)
     GPIO.output(led_red, 1)
-    time.sleep(0.1)
+    sleep_and_wait_for_button_press(0.5)
     GPIO.output(led_red, 0)
-    time.sleep(0.1)
+    sleep_and_wait_for_button_press(0.5)
 def green():
     GPIO.output(led_green, 1)
     GPIO.output(led_red, 0)
@@ -32,16 +41,16 @@ state = 0
 while True:
     # print(GPIO.input(button))
     time.sleep(.1)
-    if(GPIO.input(button)==1):
+    if(GPIO.input(button)==button_pressed):
         # print("1")
-        time.sleep(0.1)
-        if(GPIO.input(button)==1):
+        time.sleep(0.05)
+        if(GPIO.input(button)==button_pressed):
             print("certain")
             state = (state+1)%4
             print(state)
-            while(GPIO.input(button)==1):
+            while(GPIO.input(button)==button_pressed):
                 pass
-            time.sleep(.1)
+            time.sleep(.05)
 
     if(state==0):   
         red()
